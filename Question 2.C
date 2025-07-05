a06c2a378aa29bade3ddb22134c0b85d4cae9e46
mugashaarnold <mugashaarnold19@gmail.com>
2025-07-05 13:06:29 +0300

    Create Question 2.C

---
+++ Question 2.C
@@ -0,0 +1,5 @@
+String author1 = "Ainebyoona"; 
+String author2 = "ainebyoona"; 
+String author3 = new String("Ainebyoona"); 
+System.out.println(author1 == author3); // false
+System.out.println(author1.equalsIgnoreCase(author2)); // true
